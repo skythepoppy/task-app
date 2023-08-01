c6144ce54d6b6d0422538143631da9f7fba5ca41
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-08-01 16:24:23 -0500

    Implement checkbox to complete task items feature

--- web.py
+++ web.py
@@ -12,10 +12,16 @@ def add_todo():
 st.title("My To-Do App")
 st.subheader("Manage your tasks with this simple To-Do app.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label = "Enter a task",
               placeholder = "Add new task",
               on_change=add_todo,
               key='new_todo')
+
